a765c2e3c267df79ad4b571a86fb51f1e9bac961
cryptochassis <admin@cryptochassis.com>
2020-10-02 13:55:05 -0700

    improve event queue

--- include/ccapi_cpp/ccapi_event_queue.h
+++ include/ccapi_cpp/ccapi_event_queue.h
@@ -3,45 +3,48 @@
 #include <queue>
 #include <mutex>
 #include <vector>
+#include <condition_variable>
 #include "ccapi_cpp/ccapi_event.h"
 namespace ccapi {
 class EventQueue final {
  public:
   void push(Event&& event) {
-    if (this->maxSize == -1 || this->queue.size() < this->maxSize) {
+    {
       std::lock_guard<std::mutex> lock(this->lock);
-      CCAPI_LOGGER_TRACE("this->queue.size() = "+size_tToString(this->queue.size()));
-      this->queue.push_back(event);
-    } else {
-      CCAPI_LOGGER_WARN("event queue is full!");
+      if (this->maxSize == -1 || this->queue.size() < this->maxSize) {
+        CCAPI_LOGGER_TRACE("this->queue.size() = "+size_tToString(this->queue.size()));
+        this->queue.push_back(event);
+      } else {
+        CCAPI_LOGGER_WARN("event queue is full!");
+      }
     }
+    this->cv.notify_all();
   }
   Event& front() {
     std::lock_guard<std::mutex> lock(this->lock);
     return this->queue.front();
   }
   std::vector<Event> purge() {
-    std::unique_lock<std::mutex> lock(this->lock);
+    std::lock_guard<std::mutex> lock(this->lock);
     std::vector<Event> p;
     std::swap(p, this->queue);
-    lock.unlock();
     return p;
   }
   size_t size() const {
+    std::lock_guard<std::mutex> lock(this->lock);
     return this->queue.size();
   }
-
   size_t getMaxSize() const {
     return maxSize;
   }
-
   void setMaxSize(size_t maxSize) {
     this->maxSize = maxSize;
   }
+  std::condition_variable cv;
 
  private:
   std::vector<Event> queue;
-  std::mutex lock;
+  mutable std::mutex lock;
   size_t maxSize{};
 };
 } /* namespace ccapi */
