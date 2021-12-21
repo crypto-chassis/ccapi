d306e3a7115170190374a33f265d4fbe1526c4f2
Crypto Chassis <admin@cryptochassis.com>
2021-12-21 15:06:27 -0800

    fix: typo

--- include/ccapi_cpp/service/ccapi_execution_management_service_bitfinex.h
+++ include/ccapi_cpp/service/ccapi_execution_management_service_bitfinex.h
@@ -223,7 +223,7 @@ class ExecutionManagementServiceBitfinex : public ExecutionManagementService {
   }
   void extractOrderInfoFromRequest(std::vector<Element>& elementList, const Request& request, const Request::Operation operation,
                                    const rj::Document& document) override {
-    if (operation == Request::Operation::CREATE_ORDER || operation == Request::Operation::CREATE_ORDER) {
+    if (operation == Request::Operation::CREATE_ORDER || operation == Request::Operation::CANCEL_ORDER) {
       Element element;
       this->extractOrderInfo(element, document[4][0]);
       elementList.emplace_back(std::move(element));
