94fe95d909db5f78f3a06b62ff5ca229d793dda2
Crypto Chassis <admin@cryptochassis.com>
2022-10-12 13:34:01 -0700

    fix: small regression

--- example/src/market_data_simple_subscription/main.cpp
+++ example/src/market_data_simple_subscription/main.cpp
@@ -1,18 +1,6 @@
 #include "ccapi_cpp/ccapi_session.h"
 namespace ccapi {
-class MyLogger final : public Logger {
- public:
-  void logMessage(const std::string& severity, const std::string& threadId, const std::string& timeISO, const std::string& fileName,
-                  const std::string& lineNumber, const std::string& message) override {
-    std::lock_guard<std::mutex> lock(m);
-    std::cout << threadId << ": [" << timeISO << "] {" << fileName << ":" << lineNumber << "} " << severity << std::string(8, ' ') << message << std::endl;
-  }
-
- private:
-  std::mutex m;
-};
-MyLogger myLogger;
-Logger* Logger::logger = &myLogger;
+Logger* Logger::logger = nullptr;  // This line is needed.
 class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
@@ -40,7 +28,7 @@ int main(int argc, char** argv) {
   SessionConfigs sessionConfigs;
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
-  Subscription subscription("okx", "BTC-USDT", "MARKET_DEPTH", argv[1]);
+  Subscription subscription("coinbase", "BTC-USD", "MARKET_DEPTH");
   session.subscribe(subscription);
   std::this_thread::sleep_for(std::chrono::seconds(10));
   session.stop();
