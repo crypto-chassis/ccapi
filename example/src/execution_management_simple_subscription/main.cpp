c8a631575316d8619dcf8ec74322b76732bdbd19
Crypto Chassis <admin@cryptochassis.com>
2023-01-30 14:30:34 -0800

    ci: revert modifications in examples

--- example/src/execution_management_simple_subscription/main.cpp
+++ example/src/execution_management_simple_subscription/main.cpp
@@ -1,18 +1,6 @@
 #include "ccapi_cpp/ccapi_session.h"
 namespace ccapi {
-class MyLogger final : public Logger {
- public:
-  void logMessage(const std::string& severity, const std::string& threadId, const std::string& timeISO, const std::string& fileName,
-                  const std::string& lineNumber, const std::string& message) override {
-    std::lock_guard<std::mutex> lock(m);
-    std::cout << threadId << ": [" << timeISO << "] {" << fileName << ":" << lineNumber << "} " << severity << std::string(8, ' ') << message << std::endl;
-  }
-
- private:
-  std::mutex m;
-};
-MyLogger myLogger;
-Logger* Logger::logger = &myLogger;
+Logger* Logger::logger = nullptr;  // This line is needed.
 class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
@@ -20,10 +8,12 @@ class MyEventHandler : public EventHandler {
       std::cout << "Received an event of type SUBSCRIPTION_STATUS:\n" + event.toStringPretty(2, 2) << std::endl;
       auto message = event.getMessageList().at(0);
       if (message.getType() == Message::Type::SUBSCRIPTION_STARTED) {
-        Request request(Request::Operation::CREATE_ORDER, "binance-usds-futures", "BTCUSDT");
+        Request request(Request::Operation::CREATE_ORDER, "coinbase", "BTC-USD");
         request.appendParam({
-            {"SIDE", "BUY"}, {"LIMIT_PRICE", "23000"}, {"QUANTITY", "0.001"}, {"timeInForce", "GTX"},
-            // {"CLIENT_ORDER_ID", "6d4eb0fb-2229-469f-873e-557dd78ac11e"},
+            {"SIDE", "BUY"},
+            {"LIMIT_PRICE", "20000"},
+            {"QUANTITY", "0.001"},
+            {"CLIENT_ORDER_ID", "6d4eb0fb-2229-469f-873e-557dd78ac11e"},
         });
         session->sendRequest(request);
       }
@@ -42,23 +32,23 @@ using ::ccapi::SessionOptions;
 using ::ccapi::Subscription;
 using ::ccapi::UtilSystem;
 int main(int argc, char** argv) {
-  // if (UtilSystem::getEnvAsString("COINBASE_API_KEY").empty()) {
-  //   std::cerr << "Please set environment variable COINBASE_API_KEY" << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // if (UtilSystem::getEnvAsString("COINBASE_API_SECRET").empty()) {
-  //   std::cerr << "Please set environment variable COINBASE_API_SECRET" << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // if (UtilSystem::getEnvAsString("COINBASE_API_PASSPHRASE").empty()) {
-  //   std::cerr << "Please set environment variable COINBASE_API_PASSPHRASE" << std::endl;
-  //   return EXIT_FAILURE;
-  // }
+  if (UtilSystem::getEnvAsString("COINBASE_API_KEY").empty()) {
+    std::cerr << "Please set environment variable COINBASE_API_KEY" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (UtilSystem::getEnvAsString("COINBASE_API_SECRET").empty()) {
+    std::cerr << "Please set environment variable COINBASE_API_SECRET" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (UtilSystem::getEnvAsString("COINBASE_API_PASSPHRASE").empty()) {
+    std::cerr << "Please set environment variable COINBASE_API_PASSPHRASE" << std::endl;
+    return EXIT_FAILURE;
+  }
   SessionOptions sessionOptions;
   SessionConfigs sessionConfigs;
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
-  Subscription subscription("binance-usds-futures", "BTCUSDT", "ORDER_UPDATE,PRIVATE_TRADE");
+  Subscription subscription("coinbase", "BTC-USD", "ORDER_UPDATE");
   session.subscribe(subscription);
   std::this_thread::sleep_for(std::chrono::seconds(10));
   session.stop();
