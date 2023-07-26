a0fb6c13d811a67daf900d1d849fbd839b83bab5
Crypto Chassis <admin@cryptochassis.com>
2023-07-26 12:59:04 -0700

    test: add more java tests

---
+++ binding/java/test/Main.java
@@ -0,0 +1,32 @@
+import com.cryptochassis.ccapi.Event;
+import com.cryptochassis.ccapi.EventHandler;
+import com.cryptochassis.ccapi.Session;
+import com.cryptochassis.ccapi.SessionConfigs;
+import com.cryptochassis.ccapi.SessionOptions;
+import com.cryptochassis.ccapi.Subscription;
+import com.cryptochassis.ccapi.SubscriptionList;
+
+public class Main {
+  static class MyEventHandler extends EventHandler {
+    @Override
+    public boolean processEvent(Event event, Session session) {
+      return true;
+    }
+  }
+  public static void main(String[] args) {
+    System.loadLibrary("ccapi_binding_java");
+    var eventHandler = new MyEventHandler();
+    var option = new SessionOptions();
+    var config = new SessionConfigs();
+    var session = new Session(option, config, eventHandler);
+    var subscriptionList = new SubscriptionList();
+    subscriptionList.add(new Subscription("okx", "BTC-USDT", "MARKET_DEPTH"));
+    session.subscribe(subscriptionList);
+    try {
+      Thread.sleep(10);
+    } catch (InterruptedException e) {
+    }
+    session.stop();
+    System.out.println("Bye");
+  }
+}
