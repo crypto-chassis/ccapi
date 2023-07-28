97fbb3afba77ce738ee8dd8878f4c41066f56377
Crypto Chassis <admin@cryptochassis.com>
2023-07-28 14:20:11 -0700

    test: add c# tests

--- binding/python/test/main.py
+++ binding/python/test/main.py
@@ -1,16 +1,9 @@
 import time
-from ccapi import EventHandler, SessionOptions, SessionConfigs, Session, Subscription, Event
+from ccapi import EventHandler, SessionOptions, SessionConfigs, Session, Subscription, Event, Request
 class MyEventHandler(EventHandler):
     def __init__(self):
         super().__init__()
     def processEvent(self, event: Event, session: Session) -> bool:
-        if event.getType() == Event.Type_SUBSCRIPTION_DATA:
-            for message in event.getMessageList():
-                print(f'Best bid and ask at {message.getTimeISO()} are:')
-                for element in message.getElementList():
-                    elementNameValueMap = element.getNameValueMap()
-                    for name, value in elementNameValueMap.items():
-                        print(f'  {name} = {value}')
         return True  # This line is needed.
 if __name__ == '__main__':
     eventHandler = MyEventHandler()
@@ -19,5 +12,10 @@ if __name__ == '__main__':
     session = Session(option, config, eventHandler)
     subscription = Subscription('coinbase', 'BTC-USD', 'MARKET_DEPTH')
     session.subscribe(subscription)
+    request = Request(Request.Operation_GET_RECENT_TRADES, 'coinbase', 'BTC-USD')
+    request.appendParam({
+        'LIMIT':'1',
+    })
+    session.sendRequest(request)
     session.stop()
     print('Bye')
