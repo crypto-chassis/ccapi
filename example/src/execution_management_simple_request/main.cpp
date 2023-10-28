b2a86aa3f760b83d223c0377eb8d2078d645c6e9
Crypto Chassis <admin@cryptochassis.com>
2023-10-28 00:14:02 -0700

    chore: revert examples

--- example/src/execution_management_simple_request/main.cpp
+++ example/src/execution_management_simple_request/main.cpp
@@ -1,18 +1,6 @@
 #include "ccapi_cpp/ccapi_session.h"
 namespace ccapi {
-class MyLogger final : public Logger {
- public:
-  void logMessage(const std::string& severity, const std::string& threadId, const std::string& timeISO, const std::string& fileName,
-                  const std::string& lineNumber, const std::string& message) override {
-    std::lock_guard<std::mutex> lock(m);
-    std::cout << threadId << ": [" << timeISO << "] {" << fileName << ":" << lineNumber << "} " << severity << std::string(8, ' ') << message << std::endl;
-  }
-
- private:
-  std::mutex m;
-};
-MyLogger myLogger;
-Logger* Logger::logger = &myLogger;
+Logger* Logger::logger = nullptr;  // This line is needed.
 class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
@@ -29,14 +17,14 @@ using ::ccapi::SessionOptions;
 using ::ccapi::toString;
 using ::ccapi::UtilSystem;
 int main(int argc, char** argv) {
-  // if (UtilSystem::getEnvAsString("BINANCE_API_KEY").empty()) {
-  //   std::cerr << "Please set environment variable BINANCE_API_KEY" << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // if (UtilSystem::getEnvAsString("BINANCE_API_SECRET").empty()) {
-  //   std::cerr << "Please set environment variable BINANCE_API_SECRET" << std::endl;
-  //   return EXIT_FAILURE;
-  // }
+  if (UtilSystem::getEnvAsString("BINANCE_API_KEY").empty()) {
+    std::cerr << "Please set environment variable BINANCE_API_KEY" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (UtilSystem::getEnvAsString("BINANCE_API_SECRET").empty()) {
+    std::cerr << "Please set environment variable BINANCE_API_SECRET" << std::endl;
+    return EXIT_FAILURE;
+  }
   std::vector<std::string> modeList = {
       "create_order", "cancel_order", "get_order", "get_open_orders", "cancel_open_orders", "get_account_balances",
   };
@@ -57,7 +45,7 @@ int main(int argc, char** argv) {
       session.stop();
       return EXIT_FAILURE;
     }
-    Request request(Request::Operation::CREATE_ORDER, "binance-usds-futures", argv[2]);
+    Request request(Request::Operation::CREATE_ORDER, "binance", argv[2]);
     request.appendParam({
         {"SIDE", strcmp(argv[3], "buy") == 0 ? "BUY" : "SELL"},
         {"QUANTITY", argv[4]},
@@ -72,7 +60,7 @@ int main(int argc, char** argv) {
       session.stop();
       return EXIT_FAILURE;
     }
-    Request request(Request::Operation::CANCEL_ORDER, "binance-usds-futures", argv[2]);
+    Request request(Request::Operation::CANCEL_ORDER, "binance", argv[2]);
     request.appendParam({
         {"ORDER_ID", argv[3]},
     });
@@ -85,40 +73,21 @@ int main(int argc, char** argv) {
       session.stop();
       return EXIT_FAILURE;
     }
-    Request request(Request::Operation::GET_ORDER, "binance-usds-futures", argv[2]);
+    Request request(Request::Operation::GET_ORDER, "binance", argv[2]);
     request.appendParam({
         {"ORDER_ID", argv[3]},
     });
     session.sendRequest(request);
   } else if (mode == "get_open_orders") {
-    // if (argc != 3) {
-    //   std::cerr << "Usage: " << argv[0] << " get_open_orders <symbol>\n"
-    //             << "Example:\n"
-    //             << "    " << argv[0] << " get_open_orders BTCUSD" << std::endl;
-    //   session.stop();
-    //   return EXIT_FAILURE;
-    // }
-    std::string localIpAddress = argv[3];
-    std::string baseUrl = argv[6];
-    {
-      Request request(Request::Operation::GET_OPEN_ORDERS, "binance-usds-futures", argv[2]);
-      request.setLocalIpAddress(localIpAddress);
-      request.setBaseUrl(baseUrl);
-      session.sendRequest(request);
+    if (argc != 3) {
+      std::cerr << "Usage: " << argv[0] << " get_open_orders <symbol>\n"
+                << "Example:\n"
+                << "    " << argv[0] << " get_open_orders BTCUSD" << std::endl;
+      session.stop();
+      return EXIT_FAILURE;
     }
-    int numRequests = std::stoi(argv[4]);
-    if (numRequests == 2) {
-      int sleepSeconds = std::stoi(argv[5]);
-      if (sleepSeconds > 0) {
-        std::this_thread::sleep_for(std::chrono::seconds(sleepSeconds));
-      }
-      {
-        Request request(Request::Operation::GET_OPEN_ORDERS, "binance-usds-futures", argv[2]);
-        request.setLocalIpAddress(localIpAddress);
-        request.setBaseUrl(baseUrl);
-        session.sendRequest(request);
-      }
-    }
+    Request request(Request::Operation::GET_OPEN_ORDERS, "binance", argv[2]);
+    session.sendRequest(request);
   } else if (mode == "cancel_open_orders") {
     if (argc != 3) {
       std::cerr << "Usage: " << argv[0] << " cancel_open_orders <symbol>\n"
@@ -127,10 +96,10 @@ int main(int argc, char** argv) {
       session.stop();
       return EXIT_FAILURE;
     }
-    Request request(Request::Operation::CANCEL_OPEN_ORDERS, "binance-usds-futures", argv[2]);
+    Request request(Request::Operation::CANCEL_OPEN_ORDERS, "binance", argv[2]);
     session.sendRequest(request);
   } else if (mode == "get_account_balances") {
-    Request request(Request::Operation::GET_ACCOUNT_BALANCES, "binance-usds-futures");
+    Request request(Request::Operation::GET_ACCOUNT_BALANCES, "binance");
     session.sendRequest(request);
   }
   std::this_thread::sleep_for(std::chrono::seconds(10));
